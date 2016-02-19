41a04d57d015d97654a3ba6baf4ec5849c45013f
David Sevcik <david.sevcik@gmail.com>
2016-02-19 14:54:39 +0000

    Screenshots

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
 var urlLib  = require('url');
+var webshot = require('webshot');
+var fs      = require('fs');
+var path    = require('path');
+var mongoose = require('mongoose');
 var router  = express.Router();
 
 
@@ -13,8 +17,9 @@ router.get('/', function(req, res) {
 
 
 router.post('/mockator/scrape', function(req, res) {
-  var url   = req.body.url;
-  var pageArgs  = { url: url };
+  var url = req.body.url;
+  var id = mongoose.Types.ObjectId();
+  var pageArgs = { _id: id, url: url };
 
   request(url, function(err, response, body) {
     if (err) {
@@ -26,12 +31,21 @@ router.post('/mockator/scrape', function(req, res) {
       pageArgs.title = $('title').text();
       pageArgs.html = $.html();
 
+
       var page = new req.models.Page(pageArgs);
       page.save(function (err) {
         if (err) {
           res.send("There was a problem adding the information to the database.");
         } else {
-          res.redirect('/');
+          var renderStream = webshot(url);
+          var screenshotPath = path.resolve('./public/mockator/page-screenshots/' + page.id + '.png');
+          var file = fs.createWriteStream(screenshotPath, { encoding: 'binary' });
+
+          renderStream.on('data', function(data) {
+            file.write(data.toString('binary'), 'binary');
+          }).on('end', function() {
+            res.redirect('/');
+          });
         }
       });
     }
